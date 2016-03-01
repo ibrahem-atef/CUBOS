8cb1a91a1b6d92d18083b6a2681ae7ac3346dced
ibrahem-atef <ibrahematef@gmail.com>
2016-03-01 16:26:49 +0200

    Added the DataEntry class
    reading from data file is still a work in progress

---
+++ DataEntry.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUBOS
+{
+    //Class Name: DataEntry
+    //Objectives: contains the methods required for reading input data files and assigining the values to the appropriate methods for further work
+    //How-to-use?: You don't create objects of the type of this class "you don't create instances with -new DataEntry()-", you only use the methods within this class directly
+    abstract class DataEntry
+    {
+        //Method Name: ReadFile
+        //Objectives: This method reads the input data file and extracts the keywords along with their corresonding values
+        //Inputs: file name
+        //Outputs: Dictionary of [key, value] pairs of type strins
+        private static Dictionary<string, string> ReadFile(string file_name)
+        {
+            //a variable to store key-value pairs
+            Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+            //Check if the specified file exists
+            if (File.Exists(file_name))
+            {
+                //an array of all the lines
+                string[] data_array = File.ReadAllLines(file_name);
+                //loop over the lines
+                for (var i = 0; i < data_array.Length; i++)
+                {
+                    //a single line line
+                    string line = data_array[i];
+
+                    int end_of_key = line.IndexOf("=");
+                    if (end_of_key >= 0)
+                    {
+                        string key = line.Substring(0, end_of_key);
+                        string value = line.Substring(end_of_key + 1);
+                        if (!dictionary.ContainsKey(key))
+                        {
+                            dictionary.Add(key, value);
+                        }
+                    }
+                }
+
+                return dictionary;
+            }
+            //If the specified data file is not found
+            else
+            {
+                Console.WriteLine("The specified file does not exist. Make sure you entered the path correctly!");
+                return null;
+            }
+        }
+
+        //Method Name: AssignValues
+        //Objectives: It gets the data from the ReadFile method and decides on which variables get values and which solvers are used or methods are called depending on these values
+        //Inputs: a string to indicate file_name
+        //Outputs: a variabe of type "SimulatorData" that contains the data read from the file in a form suitable for the use of the simulator
+        public static SimulatorData ReadData(string file_name)
+        {
+            //a dictionary to store the data read from the file in a key-value pairs format
+            Dictionary<string, string> dictionary = ReadFile(file_name);
+
+            if (dictionary != null)
+            {
+                if (dictionary.ContainsKey("solver"))
+                {
+                    string solver = dictionary["solver"];
+                }
+            }
+        }
+    }
+}
---
+++ SimulatorData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CUBOS
+{
+    class SimulatorData
+    {
+        #region Initialize Rectangular grid dimensions
+        public bool homogeneous = false;
+        public int x, y, z;
+
+        public int[] inactive_blocks;
+
+        #endregion
+
+        #region Initialize grid
+        //The gridding technique used here is for only a single row or a single column
+        public int delta_X, delta_Y, delta_Z;
+        public int[] delta_X_array, delta_Y_array, delta_Z_array;
+
+        #endregion
+
+        #region Initialize Rock properties
+        //The gridding technique used here is for the whole grid
+        public int Kx_data, Ky_data, Kz_data;
+        public double porosity, compressibility_rock;
+
+        public int[] Kx_data_array, Ky_data_array, Kz_data_array;
+        public double[] porosity_array, compressibility_rock_array;
+
+        #endregion
+
+        #region Initialize PVT
+
+        public double FVF, viscosity;
+
+        //For slightly-compressibly fluid
+        public double compressibility_fluid;
+
+        //For compressible fluid, this data is used for constructing the PVT table
+        public double[][] g_data = new double[4][];
+
+        #endregion
+
+        #region  Initialize boundary conditions
+
+        public int initial_pressure;
+
+        public double[] boundary_flow_rate;
+        public double[] boundary_pressure_x;
+        public double[] boundary_pressure_y;
+        public double[] boundary_pressure_gradient_x;
+        public double[] boundary_pressure_gradient_y;
+
+        #endregion
+
+        #region  Initialize wells data
+        public int[] well_locations;
+
+        #endregion
+
+        #region run specifications
+        TypeDefinitions.Compressibility compressibility;
+        Transmissibility.Phase phase;
+        Transmissibility.GridType grid_type;
+
+        //For compressible and slightly-compressible fluids, define the values for time steps and total simulation time
+        public double delta_t = 5;
+        public double time_max = 180;
+
+        //For compressible fluid problems
+        public double convergence_pressure = 0.001;
+        #endregion
+    }
+}
